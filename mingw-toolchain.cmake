045b488da68cbf750411c1cbd5b476a39a9df96e
guorong <guorong@zkong.com>
2025-01-02 16:53:59 +0800

    windows mingw compile

---
+++ mingw-toolchain.cmake
@@ -0,0 +1,4 @@
+
+
+
+cmake .. -DCMAKE_RC_COMPILER="C:/Qt/Tools/mingw1310_64/bin/windres.exe" -DCMAKE_C_COMPILER="C:/Qt/Tools/mingw1310_64/bin/gcc.exe" -DCMAKE_CXX_COMPILER="C:/Qt/Tools/mingw1310_64/bin/g++.exe" -DPRODUCTION_OPTIMIZATION=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="C:/Qt/6.8.1/mingw_64/lib/cmake" -G "Unix Makefiles"
